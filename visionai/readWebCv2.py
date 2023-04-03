a41af5e36c0eae71679af8a979a5e18854a9fbf6
Arunkumar Nair <arunkg99@gmail.com>
2023-04-04 00:54:09 +0530

    added vision

---
+++ visionai/readWebCv2.py
@@ -0,0 +1,40 @@
+""" import cv2
+import time
+video = cv2.VideoCapture(0)
+prev = time.time()
+while True:
+	#Do processing
+    ret, frame= video.read()
+    if ret is False:
+	    time.sleep(1)
+        #continue
+ """
+
+# import the opencv library
+import cv2
+
+
+# define a video capture object
+vid = cv2.VideoCapture(0)
+
+while(True):
+	
+	# Capture the video frame
+	# by frame
+	ret, frame = vid.read()
+
+	# Display the resulting frame
+	cv2.imshow('frame', frame)
+	
+	# the 'q' button is set as the
+	# quitting button you may use any
+	# desired button of your choice
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
+
+        
